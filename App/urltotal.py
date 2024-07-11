57bb1097ad015c1327a2034301a0ce91e23eeff3
MrKelvic <kelvicansa1@gmail.com>
2024-07-11 09:08:59 +0000

    dd

--- App/urltotal.py
+++ App/urltotal.py
@@ -9,7 +9,7 @@ class VTotalAPI():
 
 
 	def __init__(self,url):
-		self.VirusTotal_API_key = "8265aa381c6dbe04fa0c7c7f04d9284d3bc473152f188821b83c5154e035241c" # GetConfig().__api__("Virustotal")["KEY"]
+		self.VirusTotal_API_key = "" # GetConfig().__api__("Virustotal")["KEY"]
 	
 		self.url  = url
 	 
