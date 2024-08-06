b0468aa6e83120e81d38ce2688de4715d631055d
MrKelvic <kelvicansa1@gmail.com>
2024-08-06 17:04:54 +0000

    websnap shot class

--- App/WebSnapShot.py
+++ App/WebSnapShot.py
@@ -1,22 +1,38 @@
-from pyppeteer import launch
 import uuid
+import asyncio
+from pyppeteer import launch
 
 class WebSnapShot(): 
 
 
 	def __init__(self, url):
-		self.imgPath = "./images/webscans/"
 		self.url = url
 
 
-	async def run(self):
-		path = self.imgPath+str(uuid.uuid4())+".png"
-		print(path)
-		browser = await launch(headless=True)
-		page = await browser.newPage()
-		await page.goto(self.url)
-		await page.screenshot({'path':path})
-		await browser.close()
+	async def screenshot(self):
+		uid = str(uuid.uuid4())
+		path = "static/webscans/" + uid + ".png"
+		# print(path)
+		print("WbeSnapShot.py **8")
+		print(self.url)
+		
+		try:
+			browser = await launch( handleSIGINT=False,handleSIGTERM=False,handleSIGHUP=False,options={'args': ['--no-sandbox']})
+			page = await browser.newPage()
+			print(page)
+			await page.goto(self.url)
+			body_height = await page.evaluate('document.body.scrollHeight')
+			await page.setViewport({'width': 1920, 'height': body_height})
+			await page.screenshot({'path': path,'fullPage': True})
+			await browser.close()
+		except KeyboardInterrupt:
+			print("KeyboardInterrupt: Stopping the screenshot process.")
+			await browser.close()  
+			return None
+		
 		return path
+		 
 
+	async def run(self):
+		return await self.screenshot()
  
